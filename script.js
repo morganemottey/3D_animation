58b7d2368c19b156fcf3f18f236be31c42301edb
Morgane Mottey <morganemottey@MacBook-Pro-de-Morgane.local>
2022-09-19 22:23:53 +0200

    Test animation

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 // ------------------------------------- init  scene ------------------------------------- //
-let scene = new THREE.Scene()
-
 // ---- init camera
 // first parameter : cajuster le champ de vue
 // second parameter : taille de la fenetre
 // third parameter : temps
 // fourth paramter : stop le rendu si notre objet est trop loin ou trop près
-let camera = new THREE.PerspectiveCamera(35, window.innerWidth/window.innerHeight, 0.1, 3000)
-// positionnement de la camera sur l'axe de Z
-camera.position.z = 100;
-
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 0.1, 3000 );
 // ------ init rendu
-let rendu = new THREE.WebGLRenderer();
-rendu.setSize(window.innerWidth, window.innerHeight); // taille du rendu à la meme grosseur que la fenetre
-rendu.setClearColor(0x132644); // couleur du rendu
-
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight ); // taille du rendu à la meme grosseur que la fenetre
+ // couleur du rendu
 //ajout du rendu dans notre document HTML
-document.body.appendChild(rendu.domElement);
-
-// ajout du rendu de notre scene et de notre camera
-rendu.render(scene,camera);
+document.body.appendChild( renderer.domElement);
 
 // ------------------------------------- Deposer et creer la forme  ------------------------------------- //
-let forme = new THREE.Group();
-
 // creation de la forme en 3 dimensions
 // il prend 3 parametres
 // le premier : le rayon
 // le deuxieme : la grosseut du tube
 // la troiseieme : le nombre de segments
 // le 3eme: le nomnre de segments au long du tube
-let geometrie = new THREE.TorusKnotGeometry(10, 3, 100, 16);
-
+const geometrie = new THREE.BoxGeometry(10, 3, 100);
 // creation du materiel avec une couleur par defaut en arc en ciel
 // seul le wireframe a true est important dans l'objet, le reste est deja dispo
-let material = new THREE.MeshNormalMaterial({
+const material = new THREE.MeshBasicMaterial({
     color: 0xff000,
-    transparent: true,
-    opacity: 1,
-    wireframe: true,
-    wireframeLinewidth: 5,
-    wireframeLinejoin: 'round',
-    wireframeLinecap: 'round'
+    // transparent: true,
+    // opacity: 1,
+    // wireframe: true,
+    // wireframeLinewidth: 5,
+    // wireframeLinejoin: 'round',
+    // wireframeLinecap: 'round'
 })
 
-//ajout de geometrie et material à notre forme
-forme.add(new THREE.Mesh(geometrie, material));
+// ajout de geometrie et material à notre forme
+const dessin = new THREE.Mesh( geometrie, material );
+// ajout de la forme sur la scene
+scene.add( dessin );
+camera.position.z = 100;
 
-//ajout de la forme sur la scene
-scene.add(forme);
+// function animate() {
+//     requestAnimationFrame( animate );
+
+//     cube.rotation.x += 0.01;
+//     cube.rotation.y += 0.01;
+
+//     renderer.render( scene, camera );
+// };
+
+// animate();
